94d0d9ecee8037c29f07b7e60d8dd327656d7c92
Vetle Lode <vetlelode@gmail.com>
2019-02-06 07:55:30 +0100

    Fixed to conform to task requirements

--- Opg6.m
+++ Opg6.m
@@ -1,2 +1,6 @@
 clc
-res = Oppgave6(100000)*4
+est1 = Oppgave6(100)*4
+est2 = Oppgave6(1000)*4
+est3 = Oppgave6(10000)*4
+%Ekstra liten bonus som kommer nermest PI
+est4 = Oppgave6(10000000)*4
--- Oppgave6.m
+++ Oppgave6.m
@@ -3,15 +3,10 @@ res = 1;
 negative = true;
     for i = 2:k
         if mod(i,2) ~= 0
-            i = i + 1;
-            if mod(i,2) ~= 0
-                i
-                negative
+            if mod((i + 1),2) ~= 0
                 mid = 1./i;
             else 
-                i-1
-                negative
-                mid = 1./(i - 1);
+                mid = 1./(i);
             end
             if negative == true
                 res = res - mid;
